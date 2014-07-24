14a48eee9b5a2ac0316d6362d595ee4f78f1de52
Andrew Gilbert <andrew.gilbert@cern.ch>
2014-07-24 14:37:04 +0200

    first commit of standalone MVAMETPairProducer

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/ICAnalysis/MVAMETPairProducer/',1)[0])+'/cfipython/osx107_amd64_gcc462/ICAnalysis/MVAMETPairProducer')
